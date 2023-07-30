81d367e4d7ebaa2ee9ebd5f62a09a3f6bf0754b4
Пользователь <vladredsoup@gmail.com>
2023-07-30 16:54:11 +0300

    feat: implement connection to database

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "QBrowserApp.h"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
+    const QStringList& args = app.arguments();
+    for (int i = 1; i < args.count(); ++i) {
+        QUrl url(args.at(i), QUrl::TolerantMode);
+        if (!url.isValid()) {
+            qWarning("Invalid URL: %s", qPrintable(args.at(i)));
+            continue;
+        }
+        QSqlError err = QConnectionCtrl::createConnection(url.scheme(), url.path().mid(1), url.host(), url.userName(), url.password(), url.port(-1));
+        if (err.type() != QSqlError::NoError)
+            qDebug() << "Unable to open connection:" << err;
+    }
 
 #ifndef Q_OS_WINDOWS
     QApplication::setWindowIcon(QIcon(":/app_icon/app_icon.xpm"));
@@ -10,9 +20,8 @@ int main(int argc, char* argv[])
 
     QBrowserApp browser;
     browser.show();
-
-    QConnectionDialog dialog(&browser);
-    dialog.exec();
+    if (QSqlDatabase::connectionNames().isEmpty())
+        QMetaObject::invokeMethod(browser.findChild<QConnectionCtrl*>("connectionCtrl"), "showConnectionDialog", Qt::QueuedConnection);
 
     return app.exec();
 }
