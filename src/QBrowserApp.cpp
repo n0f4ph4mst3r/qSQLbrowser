2e587e8ca3f6c6d5b79047e0fe267abe182b38f3
Пользователь <vladredsoup@gmail.com>
2023-08-02 20:31:43 +0300

    refactor(QSqlCtrl): remove widget

--- src/QBrowserApp.cpp
+++ src/QBrowserApp.cpp
@@ -4,19 +4,34 @@ QBrowserApp::QBrowserApp(QWidget* parent, Qt::WindowFlags flags) : QMainWindow(p
     setCentralWidget(m_mainWidget);
     setWindowTitle(QObject::tr("QSqlBrowser"));
 
-    QSplitter* splitter = new QSplitter(Qt::Vertical, m_mainWidget);
-    QSqlCtrl* sqlctrl = new QSqlCtrl(splitter);
-    QSqlQueryPanel* querypanel = new QSqlQueryPanel(splitter);
+    QSplitter* vSplitter = new QSplitter(Qt::Vertical, m_mainWidget);
+    QSplitter* hSplitter = new QSplitter(vSplitter);
 
-    splitter->setChildrenCollapsible(false);
-    splitter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    splitter->addWidget(sqlctrl);
-    splitter->addWidget(querypanel);
-    splitter->setStretchFactor(0, 1);
-    splitter->setStretchFactor(1, 0);
+    hSplitter->setChildrenCollapsible(false);
+    m_tree = new QConnectionCtrl(hSplitter);
+    m_table = new QDbTableView(hSplitter);
+
+    hSplitter->addWidget(m_tree);
+    hSplitter->addWidget(m_table);
+    hSplitter->setStretchFactor(0, 0);
+    hSplitter->setStretchFactor(1, 1);
+    hSplitter->setSizes({ 250, 550 });
+    hSplitter->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
+
+    QSqlQueryPanel* querypanel = new QSqlQueryPanel(vSplitter);
+
+    vSplitter->setChildrenCollapsible(false);
+    vSplitter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    vSplitter->addWidget(hSplitter);
+    vSplitter->addWidget(querypanel);
+    vSplitter->setStretchFactor(0, 1);
+    vSplitter->setStretchFactor(1, 0);
 
     QVBoxLayout* mainLayout = new QVBoxLayout;
-    mainLayout->addWidget(splitter);
+    mainLayout->addWidget(vSplitter);
     m_mainWidget->setLayout(mainLayout);
     m_mainWidget->setMinimumSize(820, 625);
+
+    QObject::connect(m_tree, &QConnectionCtrl::tableModelChanged, m_table, &QDbTableView::setTableModel);
+    QObject::connect(m_tree, &QConnectionCtrl::statusMessage, [&](const QString& text) { statusBar()->showMessage(text); });
 }
--- src/QBrowserApp.h
+++ src/QBrowserApp.h
@@ -4,8 +4,12 @@
 #define BROWSER_H
 
 #include <QMainWindow>
+#include <QStatusBar>
+#include <QSplitter>
+#include <QFrame>
 
-#include "QSqlCtrl.h"
+#include "QConnectionCtrl.h"
+#include "QDbTableView.h"
 #include "QSqlQueryPanel.h"
 
 class QBrowserApp : public QMainWindow
@@ -13,6 +17,9 @@ class QBrowserApp : public QMainWindow
 	Q_OBJECT
 protected:
 	QWidget* m_mainWidget;
+
+	QConnectionCtrl* m_tree;
+	QDbTableView* m_table;
 public:
 	explicit QBrowserApp(QWidget* parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags());
 };
